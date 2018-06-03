dc9d40465957c70764a183584eb4111f51946c3c
冰河 <543347844@qq.com>
2018-06-03 22:05:56 +0800

    修改host

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,5 @@
 <template>
   <div class="container">
-    <div class="m-top-50"></div>
-      <form class="form-horizontal col-md-6">
-        <div class="form-group">
-          <label for="" class="col-md-2 control-label">接口地址:</label>
-          <div class="col-md-10">
-            <input type="email" class="form-control" id="" placeholder="">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="" class="col-md-2 control-label">商户号:</label>
-          <div class="col-md-10">
-            <input type="password" class="form-control" id="" placeholder="">
-          </div>
-        </div>
-      </form>
-      <form class="form-horizontal col-md-6">
-        <div class="form-group">
-          <label for="" class="col-md-2 control-label">机构号:</label>
-          <div class="col-md-10">
-            <input type="email" class="form-control" id="" placeholder="">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="" class="col-md-2 control-label">机构密钥:</label>
-          <div class="col-md-10">
-            <input type="password" class="form-control" id="" placeholder="">
-          </div>
-        </div>
-      </form>
-    <!-- <div>Header</div> -->
-    <ul class="nav nav-tabs">
-      <li role="presentation"><nuxt-link to="/scanPay">消费者主扫</nuxt-link></li>
-      <li role="presentation"><nuxt-link to="/initQuickPay">快捷支付</nuxt-link></li>
-      <li role="presentation"><nuxt-link to="/h5Pay">H5支付</nuxt-link></li>
-      <li role="presentation"><nuxt-link to="/query">交易查询</nuxt-link></li>
-      <li role="presentation"><nuxt-link to="/refund">交易退货</nuxt-link></li>
-    </ul>
     <nuxt/>
     <!-- <div>Footer</div> -->
   </div>
